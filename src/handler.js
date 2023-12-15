7d6113b3e4cea5019d3caffe406004749ae7a4df
rizkingrh <rizkingrh.mhd@gmail.com>
2023-12-15 16:15:54 +0700

    add handler edit book by id

--- src/handler.js
+++ src/handler.js
@@ -75,6 +75,7 @@ const addBookHandler = (request, h) => {
   return response;
 };
 
+
 const getAllBookHandler = (request, h) => {
   const response = h.response({
     status: 'success',
@@ -89,6 +90,7 @@ const getAllBookHandler = (request, h) => {
   response.code(200);
   return response;
 };
+
 
 const getBookByIdHandler = (request, h) => {
   const {bookId} = request.params;
@@ -127,9 +129,75 @@ const getBookByIdHandler = (request, h) => {
   return response;
 };
 
+const editBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const updatedAt = new Date().toISOString();
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
 
 module.exports = {
   addBookHandler,
   getAllBookHandler,
   getBookByIdHandler,
+  editBookByIdHandler,
 };
